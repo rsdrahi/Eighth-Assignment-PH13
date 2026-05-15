af0d436432176154649b96c220313bc120accee1
rsdrahi <rsdrahi13@gmail.com>
2026-05-15 15:01:42 +0600

    created on navbar heroUI avarter when the user logged in and implement the signout button when user click the signout button user logged out. this functionality is compleated.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,19 @@
 "use client";
+import { authClient } from "@/lib/auth-client";
+import { Avatar, Button } from "@heroui/react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
+
+  const userData = authClient.useSession();
+  const user = userData.data?.user
+  console.log(user, "User");
+
+  const handleSignOut = async () => {
+    await authClient.signOut();
+  }
+
   return (
     <div className="shadow">
       <nav className=" flex justify-between items-center py-3 container mx-auto w-full">
@@ -32,15 +43,31 @@ const Navbar = () => {
         </ul>
 
         <div className="flex gap-4 font-medium">
-          <ul className="flex items-center gap-5 text-sm">
+          {!user && <ul className="flex items-center gap-5 text-sm">
             <li>
               <Link href={"/signup"}>SignUp</Link>
             </li>
             <li>
               <Link href={"/signin"}>SignIn</Link>
             </li>
-          </ul>
+          </ul>}
+          {
+            user &&
+            <div className="flex gap-3">
+              <Avatar size="sm">
+                <Avatar.Image alt={user?.name}
+                  src={user?.image}
+                  referrerPolicy="no-referrer"
+                />
+                <Avatar.Fallback>{user?.name[0]}</Avatar.Fallback>
+              </Avatar>
+
+              <Button onClick={handleSignOut} size="sm" variant="danger">Sign Out</Button>
+            </div>
+          }
+
         </div>
+
       </nav>
     </div>
   );
