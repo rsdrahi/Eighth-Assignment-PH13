825b99dd95527bb5a6da95aafe5a919466534dc0
rsdrahi <rsdrahi13@gmail.com>
2026-05-16 01:50:32 +0600

    branch.js json file created.

---
+++ public/branch.js
@@ -0,0 +1,22 @@
+[
+  {
+    "id": 1,
+    "city": "Dhaka",
+    "image": "https://i.ibb.co/0jqHpnp/dhaka-library.jpg"
+  },
+  {
+    "id": 2,
+    "city": "Sylhet",
+    "image": "https://i.ibb.co/7k6w9gP/sylhet-library.jpg"
+  },
+  {
+    "id": 3,
+    "city": "Khulna",
+    "image": "https://i.ibb.co/4f8Qm3M/khulna-library.jpg"
+  },
+  {
+    "id": 4,
+    "city": "Rajshahi",
+    "image": "https://i.ibb.co/Jc6F6Y2/rajshahi-library.jpg"
+  }
+]
